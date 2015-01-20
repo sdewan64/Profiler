317436e924f24c02b860a5d8801a77c4dc0d57bc
sdewan64 <sdewan64@gmail.com>
2015-01-21 01:01:43 +0600

    Added menu buttons in Account Menu

--- app/src/main/java/com/shaheed/codewarior/checkboxdevelopers/Constants.java
+++ app/src/main/java/com/shaheed/codewarior/checkboxdevelopers/Constants.java
@@ -38,7 +38,4 @@ public class Constants {
             toast.show();
         }
     }
-
-
-
 }
--- app/src/main/java/com/shaheed/codewarior/checkboxdevelopers/MainMenuActivity.java
+++ app/src/main/java/com/shaheed/codewarior/checkboxdevelopers/MainMenuActivity.java
@@ -92,9 +92,9 @@ public class MainMenuActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
